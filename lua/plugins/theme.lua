2bab0a37c9e452343077c3333d361c9663dd45b7
work-wsl-2 <13287686871@163.com>
2026-01-30 23:21:23 +0800

    theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,17 +1,17 @@
-return {
-  {
-    "folke/tokyonight.nvim",
-    opts = {
-      -- 1. 这是子欣原来的配置，保持不变
-      style = "night",
-
-      -- 2. 这是我们要新加的颜色配置
-      on_highlights = function(hl, c)
-        hl.Folded = {
-          bg = "#22202C", -- 子欣指定的那个深色背景
-          italic = true, -- 斜体更有感觉
-        }
-      end,
-    },
-  },
-}
+-- return {
+--   {
+--     "folke/tokyonight.nvim",
+--     opts = {
+--       -- 1. 这是子欣原来的配置，保持不变
+--       style = "night",
+--
+--       -- 2. 这是我们要新加的颜色配置
+--       on_highlights = function(hl, c)
+--         hl.Folded = {
+--           bg = "#22202C", -- 子欣指定的那个深色背景
+--           italic = true, -- 斜体更有感觉
+--         }
+--       end,
+--     },
+--   },
+-- }
